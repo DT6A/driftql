ed87860de6fbe8f2fd8f79ed25ee243c7a28ca6d
Denis Tarasov <dt6cgsg@gmail.com>
2026-07-23 18:04:44 +0200

    D4RL wrapper

--- envs/d4rl_utils.py
+++ envs/d4rl_utils.py
@@ -1,4 +1,16 @@
-import d4rl
+import contextlib
+import io
+import os
+import warnings
+
+os.environ.setdefault('D4RL_SUPPRESS_IMPORT_ERROR', '1')
+warnings.simplefilter('ignore', DeprecationWarning)
+
+with warnings.catch_warnings():
+    warnings.simplefilter('ignore')
+    with contextlib.redirect_stdout(io.StringIO()), contextlib.redirect_stderr(io.StringIO()):
+        import d4rl
+import gym
 import gymnasium
 import numpy as np
 
@@ -6,9 +18,50 @@ from envs.env_utils import EpisodeMonitor
 from utils.datasets import Dataset
 
 
+class GymV21EnvCompatibility(gymnasium.Env):
+    """Adapt a D4RL/Gym v0.21-style env to the Gymnasium reset/step API."""
+
+    metadata = {}
+
+    def __init__(self, env):
+        self.env = env
+        self.observation_space = env.observation_space
+        self.action_space = env.action_space
+        self.reward_range = getattr(env, "reward_range", None)
+        self.metadata = getattr(env, "metadata", {})
+        self.spec = getattr(env, "spec", None)
+
+    @property
+    def unwrapped(self):
+        return self.env.unwrapped
+
+    def __getattr__(self, name):
+        return getattr(self.env, name)
+
+    def reset(self, *args, **kwargs):
+        warnings.simplefilter("ignore", DeprecationWarning)
+        kwargs.pop("seed", None)
+        kwargs.pop("options", None)
+        return self.env.reset(*args, **kwargs), {}
+
+    def step(self, action):
+        observation, reward, done, info = self.env.step(action)
+        truncated = bool(info.get("TimeLimit.truncated", False))
+        terminated = bool(done and not truncated)
+        return observation, reward, terminated, truncated, info
+
+    def render(self, *args, **kwargs):
+        return self.env.render(*args, **kwargs)
+
+    def close(self):
+        return self.env.close()
+
+
 def make_env(env_name):
     """Make D4RL environment."""
-    env = gymnasium.make('GymV21Environment-v0', env_id=env_name)
+    warnings.simplefilter("ignore", DeprecationWarning)
+    env = gym.make(env_name)
+    env = GymV21EnvCompatibility(env)
     env = EpisodeMonitor(env)
     return env
 
@@ -23,10 +76,13 @@ def get_dataset(
         env: Environment instance.
         env_name: Name of the environment.
     """
-    dataset = d4rl.qlearning_dataset(env)
+    dataset_env = env
+    while not hasattr(dataset_env, 'get_dataset') and hasattr(dataset_env, 'env'):
+        dataset_env = dataset_env.env
+    dataset = d4rl.qlearning_dataset(dataset_env)
 
-    terminals = np.zeros_like(dataset['rewards'])  # Indicate the end of an episode.
-    masks = np.zeros_like(dataset['rewards'])  # Indicate whether we should bootstrap from the next state.
+    terminals = np.zeros_like(dataset['rewards']) # Indicate the end of an episode.
+    masks = np.zeros_like(dataset['rewards']) # Indicate whether we should bootstrap from the next state.
     rewards = dataset['rewards'].copy().astype(np.float32)
     if 'antmaze' in env_name:
         for i in range(len(terminals) - 1):
